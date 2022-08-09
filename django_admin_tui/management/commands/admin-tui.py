918b7cab32a9e5d19c56265c17a022dc27d0544d
Cameron White <whiteceric@gmail.com>
2022-08-09 12:10:35 -0400

    hello world

--- django_admin_tui/management/commands/admin-tui.py
+++ django_admin_tui/management/commands/admin-tui.py
@@ -1,9 +1,11 @@
+import py_cui
 from django.contrib.admin import site
 from django.core.management.base import BaseCommand
-from rich.layout import layout
 
 MODEL_ADMINS = site._registry
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        pass
+        root = py_cui.PyCUI(1,1)
+        root.add_label('Hello World!', 0, 0)
+        root.start()
